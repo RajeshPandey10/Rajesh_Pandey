4fd949fd7203d9068cd13c9a61dbc89c2b320856
RajeshPandey10 <imrajesh2005@gmail.com>
2025-03-20 01:37:24 +0545

    final-changed

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { Router } from 'react-router-dom'
+import Loader from './components/Loader.jsx'
+import { Suspense } from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
